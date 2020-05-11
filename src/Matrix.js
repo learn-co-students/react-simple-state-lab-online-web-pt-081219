b99e8f9a7689071a574e8cf5a8506dbd718298c2
Leah Silber <LeahSilber@gmail.com>
2020-05-11 14:47:55 -0700

    Done.

--- src/Matrix.js
+++ src/Matrix.js
@@ -1,21 +1,143 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
+import Cell from "./Cell";
 
 export default class Matrix extends Component {
-  
-  genRow = (vals) => {
-    return vals.map(val => <div className="cell"></div>) // replace me and render a cell component instead!
-  }
-  
+  static defaultProps = {
+    values: [
+      [
+        "#F00",
+        "#F00",
+        "#F00",
+        "#F00",
+        "#F00",
+        "#F00",
+        "#F00",
+        "#F00",
+        "#F00",
+        "#F00",
+      ],
+      [
+        "#F00",
+        "#F00",
+        "#F00",
+        "#F00",
+        "#F00",
+        "#F00",
+        "#F00",
+        "#F00",
+        "#F00",
+        "#F00",
+      ],
+      [
+        "#F00",
+        "#F00",
+        "#F00",
+        "#F00",
+        "#F00",
+        "#F00",
+        "#F00",
+        "#F00",
+        "#F00",
+        "#F00",
+      ],
+      [
+        "#F00",
+        "#F00",
+        "#F00",
+        "#F00",
+        "#F00",
+        "#F00",
+        "#F00",
+        "#F00",
+        "#F00",
+        "#F00",
+      ],
+      [
+        "#F00",
+        "#F00",
+        "#F00",
+        "#F00",
+        "#F00",
+        "#F00",
+        "#F00",
+        "#F00",
+        "#F00",
+        "#F00",
+      ],
+      [
+        "#F00",
+        "#F00",
+        "#F00",
+        "#F00",
+        "#F00",
+        "#F00",
+        "#F00",
+        "#F00",
+        "#F00",
+        "#F00",
+      ],
+      [
+        "#F00",
+        "#F00",
+        "#F00",
+        "#F00",
+        "#F00",
+        "#F00",
+        "#F00",
+        "#F00",
+        "#F00",
+        "#F00",
+      ],
+      [
+        "#F00",
+        "#F00",
+        "#F00",
+        "#F00",
+        "#F00",
+        "#F00",
+        "#F00",
+        "#F00",
+        "#F00",
+        "#F00",
+      ],
+      [
+        "#F00",
+        "#F00",
+        "#F00",
+        "#F00",
+        "#F00",
+        "#F00",
+        "#F00",
+        "#F00",
+        "#F00",
+        "#F00",
+      ],
+      [
+        "#F00",
+        "#F00",
+        "#F00",
+        "#F00",
+        "#F00",
+        "#F00",
+        "#F00",
+        "#F00",
+        "#F00",
+        "#F00",
+      ],
+    ],
+  };
+
+  genRow = (cells) => {
+    return cells.map((cell) => <Cell value={cell} />);
+  };
+
   genMatrix = () => {
-    return this.props.values.map(rowVals => <div className="row">{this.genRow(rowVals)}</div>)
-  }
-  
+    return this.props.values.map((cells) => (
+      <div className="row">{this.genRow(cells)}</div>
+    ));
+  };
+
   render() {
-    return (
-      <div id="matrix">
-        {this.genMatrix()}
-      </div>
-    )
+    return <div id="matrix">{this.genMatrix()}</div>;
   }
-  
 }
